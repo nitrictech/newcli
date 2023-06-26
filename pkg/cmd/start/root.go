1ab6459a0ebafc1f393f5019e0a20b4822aec728
Tim Holm <tim.holm@nitric.io>
2023-06-26 13:49:05 +1000

    print config warnings for run and start.

--- pkg/cmd/start/root.go
+++ pkg/cmd/start/root.go
@@ -108,7 +108,7 @@ var startCmd = &cobra.Command{
 
 		pterm.DefaultBasicText.Println("Local running, use ctrl-C to stop")
 
-		stackState := run.NewStackState()
+		stackState := run.NewStackState(proj)
 
 		area, _ := pterm.DefaultArea.Start()
 		// Create a debouncer for the refresh and remove locking
@@ -125,6 +125,10 @@ var startCmd = &cobra.Command{
 				stackState.Update(pool, ls)
 
 				area.Update(stackState.Tables(9001, *ls.GetDashPort()))
+
+				for _, warning := range stackState.Warnings() {
+					pterm.Warning.Println(warning)
+				}
 			})
 		})
 
